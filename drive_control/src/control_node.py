de574a59934087eb25cab487c209037795840421
Mike Miles <mikemiles19@gmail.com>
2022-02-16 16:35:45 -0700

    local changes from rock64 (which we have been using already up until now)

--- drive_control/src/control_node.py
+++ drive_control/src/control_node.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 import rospy 
 
-from ros_pololu_servo.msg import MotorCommand
 from drive_control.msg import DriveCommand
 from std_msgs.msg import Float64
 
 class drive:
     def __init__(self):
-        self.motor_topic = '/pololu/command'
-        self.front_topic = '/front_controller/command'
-        self.back_topic = '/back_controller/command'
-        self.drive_topic = '/drive/command'
-        self.cmd_motor = MotorCommand()
+        self.motor_topic = 'commands/motor/speed'
+        self.front_topic = 'front_controller/command'
+        self.back_topic = 'back_controller/command'
+        self.drive_topic = 'drive/command'
+        self.motor_speed = Float64()
         self.cmd_front = Float64()
         self.cmd_back = Float64()
          
         #create publisher passing it the vel_topic_name and msg_type
-        self.motor_pub = rospy.Publisher(self.motor_topic, MotorCommand, queue_size = 1)
+        self.motor_pub = rospy.Publisher(self.motor_topic, Float64, queue_size = 1)
         self.front_pub = rospy.Publisher(self.front_topic, Float64, queue_size = 1)
         self.back_pub = rospy.Publisher(self.back_topic, Float64, queue_size = 1)
 
@@ -29,18 +28,18 @@ class drive:
         self.cmd_front = data.front_steer_angle
         self.cmd_back = data.rear_steer_angle
 
-        if self.motor_speed < -0.5235:
-            self.motor_speed = -0.5235
-        if self.motor_speed > 0.3499:
-            self.motor_speed = 0.3499
-        self.cmd_motor.joint_name = 'motor'
-        self.cmd_motor.position = self.motor_speed
+        #if self.motor_speed < -0.5235:
+        #    self.motor_speed = -0.5235
+        #if self.motor_speed > 0.3499:
+        #    self.motor_speed = 0.3499
+        #self.cmd_motor.joint_name = 'motor'
+        #self.cmd_motor.position = self.motor_speed
 
-        self.motor_pub.publish(self.cmd_motor)
+        self.motor_pub.publish(self.motor_speed)
         self.front_pub.publish(self.cmd_front)
         self.back_pub.publish(self.cmd_back)
             
-        rospy.sleep(0.1)
+        #rospy.sleep(0.1)
 
 if __name__ == "__main__":
 	rospy.init_node('drive_node')
